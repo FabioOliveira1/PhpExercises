dd3109f07bf4871b46794c11fcec5ea78d46bd05
FabioOliveira1 <fabioaurelio1269@gmail.com>
2017-07-26 12:42:31 -0300

    Remove useless comments

--- poo/HerancaEscolaPt2/Visitante.php
+++ poo/HerancaEscolaPt2/Visitante.php
@@ -1,25 +1,6 @@
 <?php
 	require_once "Pessoa.php";
 	class Visitante extends Pessoa{
-		// private $setor;
-		// private $trabalhando;
-
-		// public function getSetor(){
-		// 	return $this->setor;
-		// }
-		// public function setSetor($value){
-		// 	$this->setor = $value;
-		// }
-		// public function getTrabalhando(){
-		// 	return $this->trabalhando;
-		// }
-		// public function setTrabalhando($value){
-		// 	$this->trabalhando = $value;
-		// }
-
-		// public function mudarTrabalho(){
-		// 	$this->setTrabalhando(!$this->getTrabalhando());
-		// }
 
 	}
 ?>
